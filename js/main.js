e42a86eb2ca6e2587f14f348ba4392932164eaf3
prashanta0234 <prashanta0234@gmail.com>
2021-07-11 02:54:37 +0600

    2nd timepush

--- js/main.js
+++ js/main.js
@@ -26,6 +26,34 @@ $('#banner').slick({
   $('#bar2').barfiller();
   $('#bar3').barfiller();
   $('#bar4').barfiller();
+  $('.team_img_info').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		infinite: true,
+		dots: false,
+		arrows: true,
+		autoplay: true,
+  		autoplaySpeed: 1000,
+  		prevArrow:'<i class="fas fa-long-arrow-alt-left arrow"></i>',
+    nextArrow:'<i class="fas fa-long-arrow-alt-right arrow"></i>',
+  		
+  		asNavFor : '.team_slide_info',
+  		centerMode: true,
+  		centerPadding: '0px;',
+	});
+
+		$('.team_slide_info').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		dots: false,
+		arrows: false,
+		autoplay: true,
+  		autoplaySpeed: 1000,
+  		asNavFor : '.team_img_info'
+	});
+
+  
 
 
 
